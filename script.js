2fecd1f6f8a0b247f740ecac255b356735bc9942
flashnoah <4accla@gmail.com>
2024-08-19 12:52:52 +0200

    Nav leiste update

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ $(window).load(function () {
         $('.hover_bkgr_fricc').hide();
     });
 });
+
+$(document).ready(function () {
+    $(".trigger_popup_fricc").click(function () {
+        $('.hover_bkgr_fricc').show();
+    });
+
+    $('.hover_bkgr_fricc').click(function () {
+        $('.hover_bkgr_fricc').hide();
+    });
+
+    $('.popupCloseButton').click(function () {
+        $('.hover_bkgr_fricc').hide();
+    });
+
+    $("#nav-toggle").click(function () {
+        $("#nav-links").toggleClass("show");
+    });
+});
